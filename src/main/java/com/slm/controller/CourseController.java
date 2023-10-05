4bb14bd74a09539dd46a9a65ac7dd5aff28008a8
Salomao <salomao.machado.75@gmail.com>
2023-10-04 22:55:30 -0300

    aula 21 - Utilizando metodo PostMapping

--- src/main/java/com/slm/controller/CourseController.java
+++ src/main/java/com/slm/controller/CourseController.java
@@ -2,8 +2,12 @@ package com.slm.controller;
 
 import java.util.List;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.slm.model.Course;
@@ -21,5 +25,13 @@ public class CourseController {
     @GetMapping
     public List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    //@RequestMapping(method = RequestMethod.POST)
+    @PostMapping
+    @ResponseStatus(code = HttpStatus.CREATED)
+    public Course create(@RequestBody Course course){
+        return courseRepository.save(course);
+    //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course))
     }
 }
