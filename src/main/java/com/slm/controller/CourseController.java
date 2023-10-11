89e480aefd8727a2ae19d20910c6825719f06b16
Salomao <salomao.machado.75@gmail.com>
2023-10-10 22:49:18 -0300

    aula 26 crud adngular e srping : Refatorando projeto

--- src/main/java/com/slm/controller/CourseController.java
+++ src/main/java/com/slm/controller/CourseController.java
@@ -3,10 +3,13 @@ package com.slm.controller;
 import java.util.List;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,11 +23,18 @@ import lombok.AllArgsConstructor;
 @AllArgsConstructor
 public class CourseController {
 
-    private CourseRepository courseRepository;
+    private final CourseRepository courseRepository;
 
     @GetMapping
-    public List<Course> list(){
+    public @ResponseBody List<Course> list(){
         return courseRepository.findAll();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Course> findById(@PathVariable Long id){
+      return courseRepository.findById(id)
+        .map(record -> ResponseEntity.ok().body(record))
+        .orElse(ResponseEntity.notFound().build());
     }
 
     //@RequestMapping(method = RequestMethod.POST)
